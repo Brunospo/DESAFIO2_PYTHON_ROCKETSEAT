9b603ceb5f845ae3ad496df12803b2c29383c913
Bruno Bispo <brunobispoo25@gmail.com>
2025-08-13 12:17:02 -0300

    Adicionada a relação com a tabela de dietas

--- models/user.py
+++ models/user.py
@@ -2,6 +2,11 @@ from app import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+  __tablename__ = 'users'
+
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), nullable=False, unique=True)
   password = db.Column(db.String(128), nullable=False)
+
+# Relacionamento com ListaDeCompras
+  listas = db.relationship('Diet', backref='users', lazy=True)
