4db7f13aa17bf5d83c5c9ad94d9c9567b8f7d55a
Bruno Bispo <brunobispoo25@gmail.com>
2025-08-13 12:17:25 -0300

    Criação da tabela de dietas

---
+++ models/diet.py
@@ -0,0 +1,12 @@
+from app import db
+
+class Diet(db.Model):
+  __tablename__ = 'diets'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), nullable=False)
+  description = db.Column(db.Text)
+  date_hour = db.Column(db.DateTime)
+  is_it_on_the_diet = db.Column(db.Boolean, default=False)
+
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
