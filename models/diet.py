2cb3524736b8eeda43eb8cf50fb83357d79f7b1c
Bruno Bispo <brunobispoo25@gmail.com>
2025-08-13 20:23:15 -0300

    Criação do metodo para ajudar a listar as dietas

--- models/diet.py
+++ models/diet.py
@@ -10,3 +10,13 @@ class Diet(db.Model):
   is_it_on_the_diet = db.Column(db.Boolean, default=False)
 
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+  def to_dict(self):
+    return {
+        "id": self.id,
+        "name": self.name,
+        "description": self.description,
+        "date_hour": self.date_hour.strftime("%d/%m/%Y %H:%M") if self.date_hour else None,
+        "is_it_on_the_diet": self.is_it_on_the_diet,
+        "user_id": self.user_id
+    }
